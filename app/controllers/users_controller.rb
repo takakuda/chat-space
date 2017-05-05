c5bf192fd11b55bbb15500b57b3ac89b1a7df2ee
kei <kei.takakuda.0415@icloud.com>
2017-05-05 15:49:09 +0900

    User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to :root
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
   resources :messages
+  resources :users
   devise_for :user
 end
