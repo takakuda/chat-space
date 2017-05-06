8508e9d9037d79b4fd84dcc685fe2d97ef065a59
kei <kei.takakuda.0415@icloud.com>
2017-05-06 15:35:49 +0900

    Add ja

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
-      config.i18n.default_locale = :ja
       g.javascripts false
       g.helper false
       g.test_framework false
