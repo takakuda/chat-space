bfff5dcb9f14de1be36238a7181f13fac67c74f2
kei <kei.takakuda.0415@icloud.com>
2017-05-08 19:21:10 +0900

    Save

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,34 @@
 class GroupsController < ApplicationController
+  # before_action :set_group
   def new
-    @group = Group.new(params[:id])
+    @group = Group.new
+  end
+
+  post = @group
+
+  def index
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = Group.new()
+    group.save
   end
 
-  def post
+  def post_params
+    params.require(:post).permit(:name, {:user_ids => []})
   end
 
-  def group_params
-    params.require(@group).permit(:name, {:user_ids =>[]})
+  private
+
+  # def set_group
+  #   @group = Group.find(params[:user_id])
+  # end
+
+  def group_post_params
+     params.require(:user_ids).permit(group_id, :user_id)
   end
+
+  # def group_post_params
+  #   params.require(@group).permit(:id, :user_id, :group_id)
+  # end
 end
--- db/migrate/20170506110217_add_column_to_groups.rb
+++ db/migrate/20170506110217_add_column_to_groups.rb
@@ -1,5 +1,7 @@
 class AddColumnToGroups < ActiveRecord::Migration[5.0]
   def change
     add_column :groups, :name, :string, null: false
+
+    add_column :groups, :id, :integer, null: false
   end
 end
