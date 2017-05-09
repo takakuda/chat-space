babe0a503af15f034bcb2361617716d8feecbed2
kei <kei.takakuda.0415@icloud.com>
2017-05-09 18:02:28 +0900

    Delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,7 @@
 class GroupsController < ApplicationController
-  # before_action :set_group
   def new
     @group = Group.new(params[:id])
   end
-
-  # post = @group
-
-  # def index
-  # end
 
   def create
     @group = Group.new(post_params)
@@ -20,21 +14,9 @@ class GroupsController < ApplicationController
       end
   end
 
-  # def post_params
-  #   params.require(:post).permit(:name, { user_ids: {:[]} })
-  # end
-
   private
-
-  # def set_group
-  #   @group = Group.find(params[:user_id])
-  # end
 
   def post_params
       params.require(:group).permit(:name, {user_ids: :[]})
   end
-
-  # def group_post_params
-  #   params.require(@group).permit(:id, :user_id, :group_id)
-  # end
 end
