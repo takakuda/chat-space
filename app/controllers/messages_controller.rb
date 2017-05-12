752094b8a770b4f5a44c90d64533bcf3ec73d70e
kei <kei.takakuda.0415@icloud.com>
2017-05-12 13:46:27 +0900

    Create action create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,17 @@ class MessagesController < ApplicationController
     @groups = current_user.groups.order(id: :DESC).limit(5)
     @group = Group.find(params[:group_id])
     @users = @group.users
+    @message = Message.new
   end
 
+  def create
+    @message = current_user.messages.new(message_params)
+    @message.save
+    redirect_to action: :index
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
+  end
 end
