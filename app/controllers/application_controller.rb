3309881f2813abb1c0cf4bc996e5ef6cf456698e
kei <kei.takakuda.0415@icloud.com>
2017-05-12 13:41:13 +0900

    Create form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  def index
+    @groups = current_user.groups.order(id: :DESC).limit(5)
+    @group = Group.find(params[:group_id])
+    @users = @group.users
+  end
+
   def configure_permitted_parameters
        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
