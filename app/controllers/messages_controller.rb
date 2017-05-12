df56f5b37a9cd9bd1cb2b8d5439a516c6a889d8b
kei <kei.takakuda.0415@icloud.com>
2017-05-12 15:41:04 +0900

    Message save

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,25 @@
 class MessagesController < ApplicationController
-
+before_action :reload_date
   def index
-    @groups = current_user.groups.order(id: :DESC).limit(5)
-    @group = Group.find(params[:group_id])
-    @users = @group.users
-    @message = Message.new
+    
   end
 
   def create
-    @message = current_user.messages.new(message_params)
+    @message = @messages.new(message_params)
     @message.save
     redirect_to action: :index
   end
 
   private
+
+  def reload_date
+    @groups = current_user.groups.order(id: :DESC).limit(5)
+    @group = Group.find(params[:group_id])
+    @users = @group.users
+    @message = Message.new
+    @messages = @group.messages
+  end
+
   def message_params
     params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
   end
