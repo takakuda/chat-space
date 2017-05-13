7c854885ca7195854d38ff590759a76e41d60031
kei <kei.takakuda.0415@icloud.com>
2017-05-13 20:13:09 +0900

    Change instance

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,7 +4,7 @@ describe Message do
   describe '#create' do
     let(:message) { FactoryGirl.build(:message)}
     it "空欄ではメッセージが送信できないこと" do
-      message = build(:message, body: "")
+      message[:body] = ""
       message.valid?
       expect(message.errors[:body]).to include("本文を入力してください。")
     end
