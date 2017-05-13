59cec7d4438ca2528f6dbdd64674f68c35121395
kei <kei.takakuda.0415@icloud.com>
2017-05-13 19:35:15 +0900

    Add let

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 require 'rails_helper'
 describe Message do
   describe '#create' do
-    let(:message){build(:message, body: "") }
+    let(:message) { FactoryGirl.build(:message)}
     it "空欄ではメッセージが送信できないこと" do
+      message = build(:message, body: "")
       message.valid?
       expect(message.errors[:body]).to include("本文を入力してください。")
     end
 
     it "メッセージが入力されていれば送信できること" do
-      message = build(:message)
       message.valid?
       expect(message).to be_valid
     end
