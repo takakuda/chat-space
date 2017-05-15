80e4484a4f4731741d3d5cfe0ab1ab6b0f15689d
kei <kei.takakuda.0415@icloud.com>
2017-05-15 14:23:17 +0900

    Comment out before action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-before_action :set_group, :set_message
+# before_action :set_group, :set_message
   def index
     @groups = current_user.groups.order(id: :DESC).limit(5)
   end
