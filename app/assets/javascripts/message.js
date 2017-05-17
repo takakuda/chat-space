60da7078be71a8e30a9180736ca7090d0ab76b37
kei <kei.takakuda.0415@icloud.com>
2017-05-17 20:55:11 +0900

    Delete turbolinks

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,38 +1,63 @@
 $(function() {
   function build(message) {
-    var html = {};
-    html['html_user'] = $('.chat-main__contents-user').html('message.user.name');
-    html['html_data'] = $('.chat-main__contents-time').html('message.created_at');
-    html['html_body'] = $('.chat-main__contents-messages').html('message.body');
-    return html;
-  }
+    var html =
 
-$('.js-form').on('submit', function(e) {
+    `<div class = "chat-main__contents-user">
+      <p>
+      &{message.name}
+      </p>
+      </div>
+      <div class = "hat-main__contents-time"
+      <p>
+      ${message.time}
+      </p>
+      </div>
+      <div class = "hat-main__contents-messages">
+      <p>
+      ${message.body}
+      </p>
+      </div>`
+
+      return html;
+    }
+
+  //   html['html_user'] = $('.chat-main__contents-user').html('message.user.name');
+  //   html['html_data'] = $('.chat-main__contents-time').html('message.created_at');
+  //   html['html_body'] = $('.chat-main__contents-messages').html('message.body');
+  //   return html;
+  // }
+
+$('#new_message').on('submit', function(e) {
   e.preventDefault();
-  var message = $(this)
-  var fd = new FormData(message[0])
-  var textField = $('#message_body');
-  var message = textField.val();
+  // var message = $(this)
+  // var fd = new FormData(message[0])
+  // var textField = $('#message_body');
+  // var message = textField.val();
+  var form = $('#message_body');
+  var message = new FormData($('#new_message').get(0));
 
   $.ajax({
     type: 'POST',
-    url: './message',
-    data: fd,
+    url: location.href,
+    // data: fd,
+    date: message,
     processData: false,
     contentType: false,
     dateType: 'json'
   })
 
   .done(function(data) {
-    var view = buildHTML(data);
-       $('.chat ').append(view);
-       $('.js-form__text-field').val('');
-       $("input").prop("disabled", false)
+    var html = buildHTML(data);
+
+       $('.chat-main__contents').append(html);
+       // $('.js-form__text-field').val('');
+       // $("input").prop("disabled", false)
+       form.val('');
      })
 
   .fail(function(data) {
     alert('メッセージが入力されていません');
   });
-
+  return false;
 });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
-json.name @message.user.name
-json.data @message.created_at
-json.body @message.body
+json.set! :name, @message.user.name
+json.set! :time, @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.set! :body, @message.body
+json.set! :id,   @message.id
