ca4856d87b32eed6228988e7205f3666d659f777
kei <kei.takakuda.0415@icloud.com>
2017-05-22 19:40:58 +0900

    Create search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
     redirect_to :root
   end
 
+  def search
+    @users = User.where('name like?', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.json 
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name,:email,:password)
