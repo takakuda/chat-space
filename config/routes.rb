4168025fe69be32903bd4f9ee0a806b76a55e0df
kei <kei.takakuda.0415@icloud.com>
2017-05-22 20:03:06 +0900

    Add route search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :user
   resources :users, only: [:edit, :update]
   resources :groups, except: [:destroy, :show] do
+    collection do
+      get 'search'
+    end
     resources :messages, only: [:index, :create]
   end
 end
