d4d75c5b97f0b53e1a2e96527c90a872b0a07760
kei <kei.takakuda.0415@icloud.com>
2017-05-23 20:02:00 +0900

    Carrierwaveを導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
-class Message < ApplicationRecord
+class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
   validates :body, presence: true
+
+  mount_uploader :image, ImageUploader
 end
